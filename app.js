d5939467765b6bcc3b8e83e4278c9eae83adfd15
hoa0 <t-hoa.l@hotmail.com>
2022-04-01 10:14:39 +0200

    :poop: install npm and render

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+//const dotenv = require("dotenv").config();
+const port = process.env.port || 3003;
+
+app.use(express.static('static'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+            res.render('index', {
+                title: 'overzicht iets',
+            })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
