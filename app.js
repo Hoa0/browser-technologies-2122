a3edfee3543d04e3cf06f5698b89d41efeb5156e
hoa0 <t-hoa.l@hotmail.com>
2022-04-06 14:30:08 +0200

    :poop: set up question list

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ app.use(express.static('static'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-            res.render('index', {
-                title: 'overzicht iets',
-            })
+  res.render('index', {
+    title: 'Enquete minor web development',
+  })
 })
+
+app.get("/getData", function (request, response) {
+  var firstname = request.query.firstname;
+
+  if (firstname != "") {
+    response.send("Your email address is " + firstname + "@gullele.com");
+  } else {
+    response.send("Please provide us first name");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
